903b13e3bab582885ee9973bc440b2d7169a8c54
LBRN <111892485+Panginoon@users.noreply.github.com>
2024-11-03 21:41:14 +0800

    Add files via upload

---
+++ midterm-ipt/database/database.php
@@ -0,0 +1,30 @@
+<?php
+
+require_once('db_credentials.php'); // getting credentials from file db_credentials.php
+
+function db_connect() { // function to connect your form to your database
+    try {
+        $dsn = "mysql:host=" . DB_SERVER . ";dbname=" . DB_NAME . ";charset=utf8mb4"; // Data Source Name type of db
+        $options = [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, // Enable exceptions
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, // Default fetch mode
+            PDO::ATTR_EMULATE_PREPARES => false, // Disable emulation of prepared statements
+        ];
+        $connection = new PDO($dsn, DB_USER, DB_PASS, $options); // Connecting to the database using PDO
+        return $connection;
+    } catch (PDOException $e) {
+        exit("Database connection failed: " . $e->getMessage());
+    }
+}
+
+function db_disconnect($connection) { // function to disconnect the form from the database
+    $connection = null; // Set the connection to null to help with garbage collection
+}
+function confirm_result_set($result_set) {  // check query
+    if (!$result_set) {
+        exit("Database query failed.");
+    }
+}
+// connection to mysql db handling errors, and ensuring queries are successful.
+
+?>
